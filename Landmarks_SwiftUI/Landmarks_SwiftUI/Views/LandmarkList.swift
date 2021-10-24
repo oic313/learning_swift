985e197d9a34a2b32117171ea589e0a6a49c5d97
oic313 <nowalicht313kn@gmail.com>
2021-10-24 18:12:56 +0900

    SwiftUIチュートリアル Building Lists and Navigation section5 リストを動的に出力するように変更

--- Landmarks_SwiftUI/Landmarks_SwiftUI/Views/LandmarkList.swift
+++ Landmarks_SwiftUI/Landmarks_SwiftUI/Views/LandmarkList.swift
@@ -2,11 +2,22 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        // LandmarkRowをリストにして出力
-        List {
-            LandmarkRow(landmark: landmarks[0])
-            LandmarkRow(landmark: landmarks[1])
+//        // LandmarkRowをリストにして出力
+//        List {
+//            LandmarkRow(landmark: landmarks[0])
+//            LandmarkRow(landmark: landmarks[1])
+//        }
+        
+//        // 上記を動的なリストに変更したい
+//        List(landmarks, id: \.id) { landmark in
+//            LandmarkRow(landmark: landmark)
+//        }
+        
+        // LandmarkにswiftIdentifiableを追加することで、以下のように簡略化できる
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
         }
+
     }
 }
 
