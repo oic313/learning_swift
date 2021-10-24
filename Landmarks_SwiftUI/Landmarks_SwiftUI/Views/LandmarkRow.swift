1f8071e924e3c8c27dbd86d6219feab83496978e
oic313 <nowalicht313kn@gmail.com>
2021-10-24 22:30:30 +0900

    SwiftUIチュートリアル Handling User Input Section1 お気に入りパラメータの追加

--- Landmarks_SwiftUI/Landmarks_SwiftUI/Views/LandmarkRow.swift
+++ Landmarks_SwiftUI/Landmarks_SwiftUI/Views/LandmarkRow.swift
@@ -11,6 +11,12 @@ struct LandmarkRow: View {
             Text(landmark.name)
             
             Spacer()
+
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
+
         }
     }
 }
