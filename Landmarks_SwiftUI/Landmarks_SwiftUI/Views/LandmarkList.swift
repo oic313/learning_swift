8cae501191270d54555165913ccbf9c13eb9599a
oic313 <nowalicht313kn@gmail.com>
2021-10-24 18:04:13 +0900

    SwiftUIチュートリアル Building Lists and Navigation section4 ランドマークのリストを作成

--- Landmarks_SwiftUI/Landmarks_SwiftUI/Views/LandmarkList.swift
+++ Landmarks_SwiftUI/Landmarks_SwiftUI/Views/LandmarkList.swift
@@ -1,15 +1,12 @@
-//
-//  LandmarkList.swift
-//  Landmarks_SwiftUI
-//
-//  Created by 野口和宏 on 2021/10/24.
-//
-
 import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // LandmarkRowをリストにして出力
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
     }
 }
 
