59199a411029ba0c0114e29ea2fc7256d5a3de51
oic313 <nowalicht313kn@gmail.com>
2021-10-24 22:51:22 +0900

    SwiftUIチュートリアル Handling User Input Section3 お気に入りフィルター機能の追加 全体でインデントを揃えたので余計なdiffが出てます

--- Landmarks_SwiftUI/Landmarks_SwiftUI/Views/LandmarkList.swift
+++ Landmarks_SwiftUI/Landmarks_SwiftUI/Views/LandmarkList.swift
@@ -1,8 +1,8 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    @State private var showFavoritesOnly = true
-
+    @State private var showFavoritesOnly = false
+    
     var filteredLandmarks: [Landmark] {
         landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
@@ -10,39 +10,46 @@ struct LandmarkList: View {
     }
     
     var body: some View {
-//        // LandmarkRowをリストにして出力
-//        List {
-//            LandmarkRow(landmark: landmarks[0])
-//            LandmarkRow(landmark: landmarks[1])
-//        }
+        //        // LandmarkRowをリストにして出力
+        //        List {
+        //            LandmarkRow(landmark: landmarks[0])
+        //            LandmarkRow(landmark: landmarks[1])
+        //        }
         
-//        // 上記を動的なリストに変更したい
-//        List(landmarks, id: \.id) { landmark in
-//            LandmarkRow(landmark: landmark)
-//        }
+        //        // 上記を動的なリストに変更したい
+        //        List(landmarks, id: \.id) { landmark in
+        //            LandmarkRow(landmark: landmark)
+        //        }
         
         NavigationView {
-            // LandmarkにswiftIdentifiableを追加することで、以下のように簡略化できる
-            
-            List(filteredLandmarks) { landmark in
-                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                LandmarkRow(landmark: landmark)
+            //            // LandmarkにswiftIdentifiableを追加することで、以下のように簡略化できる
+            //            List(filteredLandmarks) { landmark in
+            // お気に入りフィルターのオンオフ切り替えに合わせてリストを切り替える
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
         }
-
+        
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-//        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
-//            LandmarkList()
-//                .previewDevice(PreviewDevice(rawValue: deviceName))
-//                .previewDisplayName(deviceName)
-//        }
+        //        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+        //            LandmarkList()
+        //                .previewDevice(PreviewDevice(rawValue: deviceName))
+        //                .previewDisplayName(deviceName)
+        //        }
         LandmarkList()
-
+        
     }
 }
